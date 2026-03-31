bd8caaaba8e40325e61d1378c4131670f52c7593
Ike <ike.cadworks@gmail.com>
2026-03-31 10:02:02 +0200

    Commentating
    more cohesiveness

--- Main.py
+++ Main.py
@@ -1,7 +1,14 @@
-import pandas as pd 
+import pandas as pd #pandas... doesn't work. still figuring it out
 
+#This part imports the 2 .csv file so that everything isn't inline scripted
 l1 = pd.read_csv("lap1.csv")
 l2 = pd.read_csv("lap2.csv")
 
+#Just to make sure, I'll print out both files by themselves before comparing. Since this works we can contineu
 print("Previous Lap:\n", l1.head())
-print("Recent Lap:\n", l2.head())
+print("Recent Lap:\n", l2.head()) 
+
+#compare in table 
+
+l2.compare(l1, keep_shape=False, keep_equal=True)
+((l1.fillna(1) == l2.fillna(1)).melt()['value'] == 0).sum()
