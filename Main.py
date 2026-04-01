10233cb163b38bfc72e006a04552aec49eb888e0
Ike <ike.cadworks@gmail.com>
2026-04-01 14:58:18 +0200

    Updated Main
    More commentation alongside attempts to make the comparison function work

--- Main.py
+++ Main.py
@@ -1,14 +1,21 @@
+from pdb import run
+
 import pandas as pd #pandas... doesn't work. still figuring it out
 
-#This part imports the 2 .csv file so that everything isn't inline scripted
 l1 = pd.read_csv("lap1.csv")
 l2 = pd.read_csv("lap2.csv")
 
-#Just to make sure, I'll print out both files by themselves before comparing. Since this works we can contineu
+#Just to make sure, I'll print out both files by themselves before comparing. Since this works we can continue
+
 print("Previous Lap:\n", l1.head())
 print("Recent Lap:\n", l2.head()) 
 
-#compare in table 
+#here is where the comparison will happen. For now it uses static information from the current CSV files
+#At some point I will make it possible to compare files that are not hard coded in, but for now this is just to get the function working.
+print("Comparing Laps...")
 
-l2.compare(l1, keep_shape=False, keep_equal=True)
-((l1.fillna(1) == l2.fillna(1)).melt()['value'] == 0).sum()
+def compare():
+    pd.merge(l1, l2, on='speed', how='left', indicator=True).query("_merge == 'left_only'")
+    pd.merge(l1, l2, on='speed', how='right', indicator=True).query("_merge == 'right_only'")
+
+print(compare())
