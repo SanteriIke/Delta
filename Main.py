f5ce3873f52cc0033189879de5556a9c72671393
Ike <ike.cadworks@gmail.com>
2026-04-08 08:58:57 +0200

    Main.py and README
    Highlighting function (broken) added, README grammar corrected as well

--- Main.py
+++ Main.py
@@ -1,21 +1,24 @@
 from pdb import run
 
-import pandas as pd #pandas... doesn't work. still figuring it out
-
-l1 = pd.read_csv("lap1.csv")
-l2 = pd.read_csv("lap2.csv")
+import pandas as pd #only works in visual studio code, tried in normal visual studio.
 
 #Just to make sure, I'll print out both files by themselves before comparing. Since this works we can continue
-
+l1 = pd.read_csv("lap1.csv")
+l2 = pd.read_csv("lap2.csv")
 print("Previous Lap:\n", l1.head())
 print("Recent Lap:\n", l2.head()) 
+
+def highlight_differences(df):
+    # currently trying to highlight differences
+    def highlight(val):
+        color = 'red' if val else ''
+        return f'background-color: {color}'
+
+    return df.applymap(highlight)
 
 #here is where the comparison will happen. For now it uses static information from the current CSV files
 #At some point I will make it possible to compare files that are not hard coded in, but for now this is just to get the function working.
 print("Comparing Laps...")
+delta = l1.compare(l2)
+print("Comparison:\n", delta) 
 
-def compare():
-    pd.merge(l1, l2, on='speed', how='left', indicator=True).query("_merge == 'left_only'")
-    pd.merge(l1, l2, on='speed', how='right', indicator=True).query("_merge == 'right_only'")
-
-print(compare())
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Delta
 A simple lap data analyzer with mock data. 
 The basic premise is that you have your sector's top speed, thottle input and brake input. 
-Input between 0.0-1.0 just being how much pressue is applied on either
+Input between 0.0-1.0 is how much pressue is applied on either
 If the speed or input are the same, then the output will be NaN
 If one is greater than the other then it will be highlighted in green. With more than 2 laps, the fastest sector will be purple as standard
 
